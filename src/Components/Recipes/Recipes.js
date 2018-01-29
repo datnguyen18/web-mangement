cba6ec57f02f925b18e70f604dd6352a66c2a7b0
tuhuynh94 <tu.huynh.k3set@eiu.edu.vn>
2018-01-29 18:29:47 +0700

    update file css + recipes

---
+++ src/Components/Recipes/Recipes.js
@@ -0,0 +1,224 @@
+import React, { Component } from "react";
+
+import { firebaseApp } from "../../config/Firebase";
+import FileUploader from "react-firebase-file-uploader";
+import { Row, Col, Card, CardHeader, CardBlock, Button } from "reactstrap";
+import "./Recipes.css";
+
+class ManageItems extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      price: "",
+      imageUrl: "",
+      isSoftDrink: false,
+      category: "",
+      inputIngredient: ["input"],
+      ingredientNames: [],
+      ingredient: [{ name: "", quantity: "" }]
+    };
+    this.itemRef = firebaseApp.database().ref("Warehouse");
+    this.itemRefDrink = firebaseApp.database().ref("Drink");
+  }
+
+  componentWillMount() {
+    let array = [];
+    this.itemRef.child("Ingredients").once("value", snapshot => {
+      snapshot.forEach(e => {
+        array.push({ name: e.key });
+      });
+      this.setState({
+        ingredientNames: array
+      });
+    });
+  }
+
+  handleSelect = event => {
+    this.setState({
+      inputIngredient: ["input"]
+    });
+    if (event.target.value === "SoftDrink") {
+      this.setState({
+        isSoftDrink: true
+      });
+    } else {
+      this.setState({
+        isSoftDrink: false,
+        category: event.target.value
+      });
+    }
+  };
+  addInput = () => {
+    this.setState({
+      inputIngredient: this.state.inputIngredient.concat(["a"]),
+      ingredient: this.state.ingredient.concat([{ name: "", quantity: "" }])
+    });
+  };
+  handleChangeSelect = (e, index) => {
+    this.state.ingredient[index].name = e.target.value;
+  };
+
+  handleChangeInput = (e, index) => {
+    this.state.ingredient[index].quantity = e.target.value;
+  };
+  handleUploadError = error => {
+    alert(error);
+  };
+
+  handleUploadSuccess = filename => {
+    firebaseApp
+      .storage()
+      .ref("img")
+      .child(filename)
+      .getDownloadURL()
+      .then(url => this.setState({ imageUrl: url }));
+  };
+  uploadFile = async file => {
+    return new Promise((resolve, reject) => {
+      firebaseApp
+        .storage()
+        .ref("img")
+        .child(file.name)
+        .put(file)
+        .then(async function(result) {
+          await firebaseApp
+            .storage()
+            .ref("img")
+            .child(file.name)
+            .getDownloadURL()
+            .then(url => resolve(url));
+        });
+    });
+  };
+  handleUploadDrink = async () => {
+    let array = [];
+    var imgUrl = await this.uploadFile(this.state.fileImage);
+    this.state.ingredient.forEach(e => {
+      array.push({ [e.name]: e.quantity });
+    });
+    this.itemRefDrink.child(this.state.name).set({
+      price: this.state.price,
+      category: this.state.category,
+      imageUrl: imgUrl,
+      ingredient: array
+    });
+  };
+  handleChange(selectorFiles) {
+    this.state.fileImage = selectorFiles[0];
+  }
+  render() {
+    console.log(this.state.imageUrl);
+    let ingredients = null;
+    if (!this.state.isSoftDrink) {
+      ingredients = (
+        <div className="form-group row">
+          <label for="inputPassword" class="col-sm-2 col-form-label">
+            Add Ingredient
+          </label>
+          <div className="col-sm-10">
+            {this.state.inputIngredient.map((input, index) => {
+              return (
+                <div className="group-add-ingredient form-row">
+                  <div className="col-sm-5">
+                    <select
+                      className="form-control"
+                      onChange={e => this.handleChangeSelect(e, index)}
+                    >
+                      <option value="select" selected>
+                        Select
+                      </option>
+                      {this.state.ingredientNames.map((e, i) => {
+                        return <option value={e.name}>{e.name}</option>;
+                      })}
+                    </select>
+                  </div>
+
+                  <div className="col-sm-5">
+                    <input
+                      onChange={e => this.handleChangeInput(e, index)}
+                      type="text"
+                      class="form-control"
+                    />
+                  </div>
+                  <div className="col-sm-2">
+                    <Button onClick={this.addInput} color="primary" id="add">
+                      +
+                    </Button>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      );
+    }
+    return (
+      <div className="container col-sm-6">
+        <form>
+          <div class="form-group row">
+            <label for="staticEmail" class="col-sm-2 col-form-label">
+              Name
+            </label>
+            <div class="col-sm-10">
+              <input
+                onChange={e => {
+                  this.setState({ name: e.target.value });
+                }}
+                type="text"
+                class="form-control"
+              />
+            </div>
+          </div>
+          <div class="form-group row">
+            <label for="inputPassword" class="col-sm-2 col-form-label">
+              Price
+            </label>
+            <div class="col-sm-10">
+              <input
+                onChange={e => {
+                  this.setState({ price: e.target.value });
+                }}
+                type="text"
+                class="form-control"
+              />
+            </div>
+          </div>
+          <div class="form-group row">
+            <label for="inputPassword" class="col-sm-2 col-form-label">
+              Image
+            </label>
+            <div class="col-sm-10">
+              <div>
+                <input
+                  type="file"
+                  accept="image/*"
+                  onChange={e => this.handleChange(e.target.files)}
+                />
+              </div>
+            </div>
+          </div>
+          <div class="form-group row">
+            <label for="inputPassword" class="col-sm-2 col-form-label">
+              Category
+            </label>
+            <select style={{ marginLeft: 15 }} onChange={this.handleSelect}>
+              <option value="Coffee">Coffee</option>
+              <option value="Itanlian Soda">Italian Soda</option>
+              <option value="Smoothies">Smoothies</option>
+              <option value="SoftDrink">Soft Drink</option>
+            </select>
+          </div>
+          {ingredients}
+          <div class="button-add col-sm-12 text-center">
+            <Button color="primary" onClick={this.handleUploadDrink}>
+              Add Drink
+            </Button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default ManageItems;
