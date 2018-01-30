897798342f25520b620c701caca6dc89a68bcc2a
tuhuynh94 <tu.huynh.k3set@eiu.edu.vn>
2018-01-30 13:59:14 +0700

    Update manage item

--- src/Components/ManageItems/ManageItems.js
+++ src/Components/ManageItems/ManageItems.js
@@ -17,53 +17,28 @@ class ManageItems extends Component {
       inputIngredient: ["input"],
       ingredientNames: [],
       ingredient: [{ name: "", quantity: "" }],
-      recipe: []
+      drink: []
     };
     this.itemRef = firebaseApp.database().ref("Warehouse");
     this.itemRefDrink = firebaseApp.database().ref("Drink");
   }
 
   componentWillMount() {
-    let array = [];
-    this.itemRef.child("Ingredients").once("value", snapshot => {
-      snapshot.forEach(e => {
-        array.push({ name: e.key });
+    this.itemRefDrink.on('value', snapshot =>{
+      var array = [];
+      snapshot.forEach(e =>{
+        var val = e.val();
+        array.push({
+          name : e.key,
+          category : val.category,
+          imgUrl : val.imageUrl,
+          price : val.price
+        });
       });
       this.setState({
-        ingredientNames: array
+        drink : array
       });
     });
-    let array2 = []
-    let array3 = []
-
-    this.itemRefDrink
-      .on("child_added", snapshot => {
-        if (snapshot.val().category != 'SoftDrink') {
-          array2 = []
-          array3 = []
-          for (var i in snapshot.val().ingredient) {
-            var listIngre = snapshot.val().ingredient[i]
-            for (var j in listIngre) {
-              // console.log(listIngre[j])
-              array2.push({
-                name: j,
-                quantity: listIngre[j]
-              })
-            }
-
-          }
-          array3.push({
-            name: snapshot.key,
-            ingredients: array2
-          })
-          this.setState({
-            recipe: this.state.recipe.concat(array3)
-          })
-        }
-
-      })
-
-
   }
 
   handleSelect = event => {
@@ -113,7 +88,7 @@ class ManageItems extends Component {
         .ref("img")
         .child(file.name)
         .put(file)
-        .then(async function (result) {
+        .then(async function(result) {
           await firebaseApp
             .storage()
             .ref("img")
@@ -136,53 +111,6 @@ class ManageItems extends Component {
     this.state.fileImage = selectorFiles[0];
   }
   render() {
-    console.log(this.state.recipe);
-    let ingredients = null;
-    if (!this.state.isSoftDrink) {
-      
-      ingredients = (
-        <div className="form-group row">
-          <label for="inputPassword" class="col-sm-2 col-form-label">
-            Add Ingredient
-          </label>
-          <div className="col-sm-10">
-            {this.state.inputIngredient.map((input, index) => {
-              return (
-                <div className="group-add-ingredient form-row">
-                  <div className="col-sm-5">
-                    <select
-                      className="form-control"
-                      onChange={e => this.handleChangeSelect(e, index)}
-                    >
-                      <option value="select" selected>
-                        Select
-                      </option>
-                      {this.state.ingredientNames.map((e, i) => {
-                        return <option value={e.name}>{e.name}</option>;
-                      })}
-                    </select>
-                  </div>
-
-                  <div className="col-sm-5">
-                    <input
-                      onChange={e => this.handleChangeInput(e, index)}
-                      type="text"
-                      class="form-control"
-                    />
-                  </div>
-                  <div className="col-sm-2">
-                    <Button onClick={this.addInput} color="primary" id="add">
-                      +
-                    </Button>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-      );
-    }
-
     return (
       <div className="container">
         <div className="container col-sm-6">
@@ -190,7 +118,7 @@ class ManageItems extends Component {
             <div class="form-group row">
               <label for="staticEmail" class="col-sm-2 col-form-label">
                 Name
-            </label>
+              </label>
               <div class="col-sm-10">
                 <input
                   onChange={e => {
@@ -204,7 +132,7 @@ class ManageItems extends Component {
             <div class="form-group row">
               <label for="inputPassword" class="col-sm-2 col-form-label">
                 Price
-            </label>
+              </label>
               <div class="col-sm-10">
                 <input
                   onChange={e => {
@@ -218,7 +146,7 @@ class ManageItems extends Component {
             <div class="form-group row">
               <label for="inputPassword" class="col-sm-2 col-form-label">
                 Image
-            </label>
+              </label>
               <div class="col-sm-10">
                 <div>
                   <input
@@ -232,7 +160,7 @@ class ManageItems extends Component {
             <div class="form-group row">
               <label for="inputPassword" class="col-sm-2 col-form-label">
                 Category
-            </label>
+              </label>
               <select style={{ marginLeft: 15 }} onChange={this.handleSelect}>
                 <option value="Coffee">Coffee</option>
                 <option value="Itanlian Soda">Italian Soda</option>
@@ -240,16 +168,15 @@ class ManageItems extends Component {
                 <option value="SoftDrink">Soft Drink</option>
               </select>
             </div>
-            {ingredients}
             <div class="button-add col-sm-12 text-center">
               <Button color="primary" onClick={this.handleUploadDrink}>
                 Add Drink
-            </Button>
+              </Button>
             </div>
           </form>
         </div>
         <table className="table">
-          <thead >
+          <thead>
             <tr>
               <th scope="col">#</th>
               <th scope="col">Name</th>
@@ -259,28 +186,20 @@ class ManageItems extends Component {
             </tr>
           </thead>
           <tbody>
-            {this.state.recipe.map((item, index) => {
+            {this.state.drink.map((item, index) => {
               return (
                 <tr>
                   <th scope="row">{index + 1}</th>
                   <td>{item.name}</td>
-                  <td>
-                    {item.ingredients.map(item => {
-                      return (
-                        <p>{item.name}: {item.quantity} </p>
-                      )
-                    })}
-                  </td>
-                  <td>
-                    
-                  </td>
+                  <td><img src={item.imgUrl} id="img"></img></td>
+                  <td>{item.category}</td>    
+                  <td>{item.price}đ</td>           
                 </tr>
-              )
+              );
             })}
           </tbody>
         </table>
       </div>
-
     );
   }
 }
